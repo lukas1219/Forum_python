d5f45e39421b835c479e000e00a309efad526ba9
lukas1219 <77293442+lukas1219@users.noreply.github.com>
2022-07-30 11:42:27 +0200

    update

--- forum/members/models.py
+++ forum/members/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class registieren(models.Model):
+    
+    benutzername = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    repeatpassword = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return str(self.id) + ' ' + self.benutzername + ' ' + self.email + ' ' + self.firstname + ' ' + self.lastname + ' ' + self.password + ' ' + self.repeatpassword
+    
