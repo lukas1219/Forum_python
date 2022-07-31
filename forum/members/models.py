fb1efc6494e4c4541eba470b9760fcf2a998c016
lukas1219 <77293442+lukas1219@users.noreply.github.com>
2022-07-31 17:24:13 +0200

    update

--- forum/members/models.py
+++ forum/members/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db import date
 
 class registieren(models.Model):
     benutzername = models.CharField(max_length=255)
@@ -11,3 +12,10 @@ class registieren(models.Model):
     def __str__(self):
         return str(self.id) + ' ' + self.benutzername + ' ' + self.email + ' ' + self.firstname + ' ' + self.lastname + ' ' + self.password + ' ' + self.repeatpassword
     
+class forum(models.Model):
+    created_at = models.DateField(default=date.today)
+    theaser = models.CharField(max_length=255)
+    theaseranswer = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return str(self.id) + ' ' + self.created_at + ' ' + self.theaser + ' ' + self.theaseranswer
