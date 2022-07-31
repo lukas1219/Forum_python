5f7a931c486b95c158f74efc437a374e77da9a86
lukas1219 <77293442+lukas1219@users.noreply.github.com>
2022-07-31 17:47:07 +0200

    update registrierung

--- forum/members/urls.py
+++ forum/members/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('registrierung/', views.registrierung, name='registrierung'),
     path('profil/', views.profil, name='profil'),
+    path('registrierung/addrecord/', views.addrecord, name='addrecord'),
 ]
--- forum/members/views.py
+++ forum/members/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from .models import Members
+
 
 def index(request):
   template = loader.get_template('index.html')
@@ -12,3 +15,22 @@ def registrierung(request):
 def profil(request):
     template = loader.get_template('profil.html')
     return HttpResponse(template.render({}, request))  
+  
+def index(request):
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('index.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def addrecord(request):
+  u = request.POST['benutzername']
+  v = request.POST['email']
+  w = request.POST['firstname']
+  x = request.POST['lastname']
+  y = request.POST['password']
+  z = request.POST['repeatpassword']
+  member = Members(benutzername=u, email=v, firstname=w, lastname=x, password=y, repeatpassword=z)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
