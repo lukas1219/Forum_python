a179dc499e139076d5f5e43e7b919a4a555029bd
lukas1219 <77293442+lukas1219@users.noreply.github.com>
2022-08-06 10:41:38 +0200

    update

--- forum/members/urls.py
+++ forum/members/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('registrierung/addrecord/', views.addrecord, name='addrecord'),
     path('profil/update/<int:id>', views.update, name='update'),
     path('profil/update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
-    path('newtheaser/', views.newtheaser, name='newtheaser'),
+    path('theaser/newtheaser/', views.newtheaser, name='newtheaser'),
 ]
