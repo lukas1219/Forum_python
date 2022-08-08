05456f6d649206798b6a8b60c1556d43cb4a5097
lukas1219 <77293442+lukas1219@users.noreply.github.com>
2022-08-08 12:31:22 +0200

    update

--- forum/members/apps.py
+++ forum/members/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MembersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'members'
+    
+class HomepageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Homepage'
