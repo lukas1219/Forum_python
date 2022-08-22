36b42ae8cc8d7d2672b5eed2bb58cf04e8c657e1
lukas1219 <77293442+lukas1219@users.noreply.github.com>
2022-08-22 10:12:55 +0200

    update

--- forum/members/views.py
+++ forum/members/views.py
@@ -90,7 +90,7 @@ def answer(request, id):
     }
     return HttpResponse(template.render(context, request))
   
-def theaseranswer(request):
+def theaseranswer(request, id):
   p = request.POST['theaser_answer']
   theaser = forum(theaser_answer=p)
   theaser.save()
