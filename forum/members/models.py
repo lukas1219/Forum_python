71dcceea240cabb2d03438f78d1bc009ee4f187c
lukas1218 <77293442+lukas1219@users.noreply.github.com>
2022-09-06 12:08:09 +0200

    update

--- forum/members/models.py
+++ forum/members/models.py
@@ -1,26 +1,123 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+from django_resized import ResizedImageField
+from tinymce.models import HTMLField
+from hitcount.models import HitCountMixin, HitCount
+from django.contrib.contenttypes.fields import GenericRelation
+from taggit.managers import TaggableManager
+from django.shortcuts import reverse
 
-class registieren(models.Model):
-    benutzername = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    repeatpassword = models.CharField(max_length=255)
-    
+
+User = get_user_model()
+
+class Author(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=40, blank=True)
+    slug = slug = models.SlugField(max_length=400, unique=True, blank=True)
+    bio = HTMLField()
+    points = models.IntegerField(default=0)
+    profile_pic = ResizedImageField(size=[50, 80], quality=100, upload_to="authors", default=None, null=True, blank=True)
+
     def __str__(self):
-        return str(self.id) + ' ' + self.benutzername + ' ' + self.email + ' ' + self.firstname + ' ' + self.lastname + ' ' + self.password + ' ' + self.repeatpassword
+        return self.fullname
+
+    @property
+    def num_posts(self):
+        return Post.objects.filter(user=self).count()
     
-class forum(models.Model):
-    headline = models.CharField(max_length=255)
-    theaser_text = models.CharField(max_length=255)
-    theaseranswer = models.CharField(max_length=255)
-    
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.fullname)
+        super(Author, self).save(*args, **kwargs)
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
+    description = models.TextField(default="description")
+
+    class Meta:
+        verbose_name_plural = "categories"
     def __str__(self):
-        return str(self.id) + ' ' + self.headline + ' ' + self.theaser_text
+        return self.title
     
-class answerforum(models.Model):
-     theaseranswer = models.CharField(max_length=255)
-     
-     def __str__(self):
-         return str(self.id) + ' ' + self.theaseranswer
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+    def get_url(self):
+        return reverse("posts", kwargs={
+            "slug":self.slug
+        })
+
+    @property
+    def num_posts(self):
+        return Post.objects.filter(categories=self).count()
+    
+    @property
+    def last_post(self):
+        return Post.objects.filter(categories=self).latest("date")
+
+
+class Reply(models.Model):
+    user = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:100]
+
+    class Meta:
+        verbose_name_plural = "replies"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    replies = models.ManyToManyField(Reply, blank=True)
+
+    def __str__(self):
+        return self.content[:100]
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=400)
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
+    user = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = HTMLField()
+    categories = models.ManyToManyField(Category)
+    date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    hit_count_generic = GenericRelation(HitCount, object_id_field='object_pk',
+        related_query_name='hit_count_generic_relation'
+    )
+    tags = TaggableManager()
+    comments = models.ManyToManyField(Comment, blank=True)
+    closed = models.BooleanField(default=False)
+    state = models.CharField(max_length=40, default="zero")
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return reverse("detail", kwargs={
+            "slug":self.slug
+        })
+
+    @property
+    def num_comments(self):
+        return self.comments.count()
+
+    @property
+    def last_reply(self):
+        return self.comments.latest("date")
+
+    
