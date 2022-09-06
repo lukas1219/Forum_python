0b478fc4b686dc2482feaa16c48567b7b6808315
lukas1218 <77293442+lukas1219@users.noreply.github.com>
2022-09-06 19:45:32 +0200

    update

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,18 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
-
+app_name = 'forum'
 urlpatterns = [
-    path('members/', include('members.urls')),
-    path('admin/', admin.site.urls),
-    
+    path('', views.index, name='index'),
+    path('answer/', views.answer, name='anwser'),
+    path('registrierung/', views.registrierung, name='registrierung'),
+    path('theaser_link/', views.theaser_link, name='theaser_link'),
+    path('profil/', views.profil, name='profil'),
+    path('impressum/', views.impressum, name='impressum'),
+    path('registrierung/addrecord/', views.addrecord, name='addrecord'),
+    path('profil/update/<int:id>', views.update, name='update'),
+    path('profil/update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
+    path('theaser_link/newtheaser/', views.newtheaser, name='newtheaser'),
+    path('answer/<int:id>', views.answer, name='answer'),
+   
 ]
