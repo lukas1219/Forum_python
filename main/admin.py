c99d979e1c75a21dd7003dd2d3d627365486c966
lukas1218 <77293442+lukas1219@users.noreply.github.com>
2022-09-06 20:07:24 +0200

    update

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import registieren, forum, answerforum
+from .models import Category, Author, Post, Comment, Reply
 
-
-admin.site.register(registieren)
-admin.site.register(forum)
-admin.site.register(answerforum)
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Reply)
