9db936e78f9780c47a5b7ffb3fe2b8afb1022d79
lukas1218 <77293442+lukas1219@users.noreply.github.com>
2022-09-07 13:22:10 +0200

    update

--- project/settings.py
+++ project/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'main',
+    'tinymce',
+    'hitcount',
+    'taggit',
+    'register',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
