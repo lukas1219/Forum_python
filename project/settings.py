af9c58090ad0f96170e006f3c4f838840c65aedc
lukas1218 <77293442+lukas1219@users.noreply.github.com>
2022-09-10 13:00:12 +0200

    update

--- project/settings.py
+++ project/settings.py
@@ -61,17 +61,16 @@ ROOT_URLCONF = "project.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR /"templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
                 'main.context_processors.searchFunction',
-
             ],
         },
     },
