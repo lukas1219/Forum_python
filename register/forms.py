33ec386b5019a66a91f3e4a02beaf1e0a490fdba
lukas1218 <77293442+lukas1219@users.noreply.github.com>
2022-09-19 12:56:01 -0400

    update

--- register/forms.py
+++ register/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from main.models import Author
 
 class UpdateForm(forms.ModelForm):
-    
     class Meta:
         model = Author
         fields = ("fullname", "bio", "profile_pic")
