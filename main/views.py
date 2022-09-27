43aa612bc41f8111d908665b64a58e8c11526892
lukas1218 <77293442+lukas1219@users.noreply.github.com>
2022-09-27 11:51:22 +0200

    update

--- main/views.py
+++ main/views.py
@@ -54,6 +54,7 @@ def detail(request, slug):
 
     return render(request, "detail.html", context)
 
+
 def posts(request, slug):
     category = get_object_or_404(Category, slug=slug)
     posts = Post.objects.filter(approved=True, categories=category)
--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,11 @@
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include("main.urls")),
     path('tinymce/', include('tinymce.urls')),
     path('account/', include('register.urls')),
     path('hitcount/', include('hitcount.urls', namespace='hitcount')),
